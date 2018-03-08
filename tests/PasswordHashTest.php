69e106fadf1fe8a83c9e97fb8648f0e5b61a4229
Marco Benvenuto <marco.benvenuto@neoris.net>
2018-03-08 09:35:29 +0100

    tests

--- tests/PasswordHashTest.php
+++ tests/PasswordHashTest.php
@@ -1,4 +1,10 @@
 <?php 
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+define('PHPASS_HASH_STRENGTH', 8);
+define('PHPASS_HASH_PORTABLE', false);
 
 /**
 *  Corresponding Class to test YourClass class
@@ -8,7 +14,7 @@
 *
 *  @author yourname
 */
-class PasswordHashTest extends PHPUnit_Framework_TestCase{
+class PasswordHashTest extends TestCase{
 	
   /**
   * Just check if the YourClass has no syntax error 
@@ -18,7 +24,7 @@ class PasswordHashTest extends PHPUnit_Framework_TestCase{
   *
   */
   public function testIsThereAnySyntaxError(){
-	$var = new lerislentia\PasswordHash;
+	$var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
 	$this->assertTrue(is_object($var));
 	unset($var);
   }
@@ -30,10 +36,70 @@ class PasswordHashTest extends PHPUnit_Framework_TestCase{
   * any typo before you even use this library in a real project.
   *
   */
-  public function testMethod1(){
-	$var = new lerislentia\PasswordHash;
-	$this->assertTrue($var->method1("hey") == 'Hello World');
+  public function testget_random_bytes(){
+    $var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
+    $count = 10;
+	$this->assertTrue($var->get_random_bytes($count)!='');
 	unset($var);
   }
   
+  public function testsencode64(){
+    $var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
+    $input = 'input';
+    $count = 1;
+    $this->assertTrue($var->encode64($input, $count)!='');
+    unset($var);
+  }
+
+  public function testsgensalt_private(){
+    $var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
+    $password = 'password';
+    $setting = 'setting';
+    $this->assertTrue($var->gensalt_private($password, $setting)!='');
+    unset($var);
+  }
+
+  public function testscrypt_private(){
+    $var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
+    $password = 'password';
+    $setting = 'setting';
+    $this->assertTrue($var->crypt_private($password, $setting)!='');
+    unset($var);
+  }
+
+  public function testsget_random_bytes(){
+    $var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
+    $count = 16;
+    $this->assertTrue($var->get_random_bytes($count)!='');
+    unset($var);
+  }
+
+  public function testsgensalt_blowfish(){
+    $var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
+    $input = $var->get_random_bytes(16);
+    $this->assertTrue($var->gensalt_blowfish($input)!='');
+    unset($var);
+  }
+  
+  public function testsHashPassword(){
+    $var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
+    $passwrod = 'password';
+    $this->assertTrue($var->HashPassword($passwrod)!='');
+    unset($var);
+  }
+
+  
+  public function testsCheckPassword(){
+    $var = new \Lerislentia\PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE );
+    $password = 'password';
+    $stored_hash = $var->HashPassword($password);
+    $this->assertTrue($var->CheckPassword($password, $stored_hash));
+    unset($var);
+  }
+
+  
+
+
+  
+
 }
